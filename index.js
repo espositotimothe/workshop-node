b6b4cf4a6d514b582d427225b1eaaf7be36ad3bd
TIMOTHE ESPOSITO <timotheesposito@gmail.com>
2024-10-15 11:07:36 +0200

    ajout-prénoms

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const colleagues = [
+    chalk.red('Lassana'),
+    chalk.green('Karim'),
+    chalk.yellow('Gustavo'),
+    chalk.blue('Timothé')
+];
+
+
+console.log(colleagues.join(', '));
